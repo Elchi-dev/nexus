23da211b4102cbd504847d5a937344a3554ad0be
Elchi-Dev <elchi.devmail@gmail.com>
2026-05-01 08:27:51 +0200

    fix: allow useless_conversion on PyO3 macro-generated impl blocks

--- core-rs/src/bridge.rs
+++ core-rs/src/bridge.rs
@@ -2,31 +2,49 @@
 
 use pyo3::prelude::*;
 
+/// the event bus, as seen from Python.
 #[pyclass]
 pub struct EventBus;
 
 #[pymethods]
+#[allow(clippy::useless_conversion)]
 impl EventBus {
     #[new]
-    pub fn new() -> Self { Self }
+    pub fn new() -> Self {
+        Self
+    }
 
+    /// publish an event to a topic.
     pub fn publish(&self, _event: Event) -> PyResult<()> {
+        // todo: wire to bus transport
         Ok(())
     }
 
+    /// subscribe to a topic.
     pub fn subscribe(&self, _topic: &str) -> PyResult<()> {
+        // todo: wire to bus transport
         Ok(())
     }
 }
 
+impl Default for EventBus {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+/// a single event flowing through the bus.
 #[pyclass]
 #[derive(Clone)]
 pub struct Event {
-    #[pyo3(get, set)] pub topic:   String,
-    #[pyo3(get, set)] pub payload: String,
+    #[pyo3(get, set)]
+    pub topic:   String,
+    #[pyo3(get, set)]
+    pub payload: String,
 }
 
 #[pymethods]
+#[allow(clippy::useless_conversion)]
 impl Event {
     #[new]
     pub fn new(topic: String, payload: String) -> Self {
